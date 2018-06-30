5ccdb1864c9bc83b4bae6e5086d2a58d93e532a1
John-John Tedro <john@primablock.com>
2018-06-30 04:07:28 +0200

    fix move for pt! macro

--- testing/macros.rs
+++ testing/macros.rs
@@ -52,5 +52,6 @@ macro_rules! contracts {
 /// Helper macro for proptest! to build a closure suitable for passing in to `TestRunner::run`.
 #[macro_export]
 macro_rules! pt {
+  (move $($t:tt)*) => { move || proptest!($($t)*) };
   ($($t:tt)*) => { || proptest!($($t)*) };
 }
